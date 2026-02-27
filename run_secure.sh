f38d12ad1cc51dc3c2df3aa6b19944bebfd3914c
anujbhosalehpe <anuj.pradeep-bhosale@hpe.com>
2026-02-27 12:30:18 +0530

    Create run_secure.sh

---
+++ run_secure.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+REPO_URL="git@github.com:yourrepo/healthcheck.git"
+WORKDIR="/tmp/healthcheck_run"
+BRANCH="main"
+
+echo "Creating working directory..."
+rm -rf $WORKDIR
+git clone -b $BRANCH $REPO_URL $WORKDIR
+
+cd $WORKDIR || exit 1
+
+echo "Running health check..."
+bash main_health_check.sh
+
+cd || exit 1
+
+echo "Cleaning sensitive files..."
+rm -rf $WORKDIR
+
+echo "Health check completed."
+echo "Output folder retained."
